acb56d0d3527f9400fb2e43fc42c27a59c5b6e21
Oliver Ni <oliver.ni@gmail.com>
2016-12-09 20:19:45 -0800

    Added board

--- probablity.py
+++ probablity.py
@@ -1,5 +1,22 @@
-import chance, numpy as np
-def matmul(exp):
-  x = np.matrix('0 0 0 0 0; 0 0 0 0 0')
-  for i in exp:
-    x = np.dot(x,x)
+import chance
+
+board = ["Go", "Mediterranean Avenue", "Community Chest", "Baltic Avenue", "Income Tax",
+         "Reading Railroad", "Oriental Avenue", "Chance", "Vermont Avenue", "Conneticuit Avenue",
+         "Jail", "St. Charles Place", "Electric Company", "States Avenue", "Virginia Avenue",
+         "Pensylvania Railroad", "St. James Place", "Community Chest", "Tenesee Avenue", "New York Avenue",
+         "Free Parking", "Kentucky Avenue", "Chance", "Indiana Avenue", "Illinois Avenue",
+         "B & O Railroad", "Atlantic Avenue", "Ventnor Avenue", "Water Works", "Marvin Gardens",
+         "Go to Jail", "Pacific Avenue", "North Carolina Avenue", "Community Chest", "Pensylvania Avenue",
+         "Short Line", "Chance", "Park Place", "Luxury Tax", "Boardwalk"]
+
+matrix = [[
+
+]]
+
+def matmul(x, y):
+    output = []
+    for row in x:
+        nrow = []
+        for col in zip(*y):
+            nrow += [sum(a*b for a, b in zip(row, col))]
+        output += [nrow]
